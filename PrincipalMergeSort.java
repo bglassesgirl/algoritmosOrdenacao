64d2fe2b96d1f28c46e448c01bc580f8ca686f8a
Murilo Leal <127419684+Mur1love@users.noreply.github.com>
2023-04-05 10:53:05 -0300

    Add files via upload

--- PrincipalMergeSort.java
+++ PrincipalMergeSort.java
@@ -7,7 +7,8 @@ public class PrincipalMergeSort {
 public static void main(String[] args) {
 		
 		int [] vetor = {10,7,18,9,16,4};
-		int[] vetorAuxiliar = new int[vetor.length];
+		
+		int[] vetorAuxiliar = new int[vetor.length]; // assim o vetor aux sempre tera o mesmo tamanho do principal
 
 		mergeSort(vetor, vetorAuxiliar, 0, vetor.length - 1);
 		
@@ -17,18 +18,22 @@ public static void main(String[] args) {
 
 	private static void mergeSort(int[] vetor, int[] vetorAuxiliar, int inicio, int fim) {
 		if (inicio < fim) {
-			int meio = (inicio + fim) / 2;
-			mergeSort(vetor, vetorAuxiliar, inicio, meio);
-			mergeSort(vetor, vetorAuxiliar, meio + 1, fim);
-			intercalar(vetor, vetorAuxiliar, inicio, meio, fim);
+			int meio = (inicio + fim) / 2; // dividindo o vetor
+			mergeSort(vetor, vetorAuxiliar, inicio, meio); //aplicado a recursividade 
+			mergeSort(vetor, vetorAuxiliar, meio + 1, fim); // nas duas divisoes, parte esquerda e parte direita
+			
+			intercalar(vetor, vetorAuxiliar, inicio, meio, fim); // comparar as duas partes dos vetores
 		}
 	}
 
 	private static void intercalar(int[] vetor, int[] vetorAuxiliar, int inicio, int meio, int fim) {
+		
+		//passar os valores do vetor principal ao auxiliar
 		for (int cont = inicio; cont <= fim; cont++) {
 			vetorAuxiliar[cont] = vetor[cont];
 		}
-
+		
+		//definindo cada lado
 		int left = inicio;
 		int right = meio + 1;
 
